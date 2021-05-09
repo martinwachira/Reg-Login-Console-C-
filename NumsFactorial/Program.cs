16dcd2de97c91300658e118840c49972065c2c06
martin-wachira <martinnwachira@gmail.com>
2021-05-09 15:00:58 +0300

    modify factoril method

--- NumsFactorial/Program.cs
+++ NumsFactorial/Program.cs
@@ -4,13 +4,13 @@ namespace NumsFactorial
 {
     class Program
     {
+        // declare a method to calculate the factorials
         public void FactorialMethod()
         {
             int x, num, facNum;
             Console.WriteLine("Enter Number to get it's Factorial: ");
             num = Convert.ToInt32(Console.ReadLine());
             facNum = num;
-
             for (x = num - 1; x>=1; x--)
             {
                 facNum = facNum * x;
@@ -21,43 +21,38 @@ namespace NumsFactorial
         static void Main(string[] args)
         {
             Program prog = new Program();
+
+            // implement error handling here
             try
             {
                 string inputMsg;
-                Console.Clear();
                 prog.FactorialMethod();
                 Console.WriteLine("Do you want to Quit? \n 1. No \n 2. Yes \n");
                 inputMsg = Console.ReadLine();
-                //if(inputMsg == "No")
-                //{
-                //    prog.FactorialMethod();
-                //    Console.Clear();
-                //}else if(inputMsg == "Yes")
-                //{
-                //    Console.Write("Logging out...");
-                //}
-                //else
-                //{
-                //    Console.WriteLine("Error");
-                //    Console.ReadKey();
-                //}
-
-                // while loop
-                //while (inputMsg == "No")
-                //{
-                //    prog.FactorialMethod();
-                //}
 
                 switch (inputMsg)
                 {
                     case "No":
                         prog.FactorialMethod();
+                        do
+                        {
+                            Console.WriteLine("Do you want to Quit? \n 1. No \n 2. Yes \n");
+                            inputMsg = Console.ReadLine();
+                            if(inputMsg == "No")
+                            {
+                                prog.FactorialMethod();
+                            }
+                        }
+                        while (inputMsg == "No");
+                        if(inputMsg == "Yes"){
+                            Console.WriteLine("You're being loged out, Thanks for using this Program... ");
+                        }
                         break;
                     case "Yes":
                         Console.WriteLine("Logging out... ");
                         break;
                     default:
-                        Console.WriteLine("logged out");
+                        Console.WriteLine("Unknown input, Please try again");
                         break;
                 }
             }
